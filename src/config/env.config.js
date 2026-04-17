1ea6a58f85708cbcaf2b57ab45ccbf24e4347879
Tran Vo Son Tung <sontungexpt@gmail.com>
2026-04-17 18:37:14 +0700

    add default endpoint

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,5 +1,7 @@
 export const Env = {
-  BASE_API_URL: import.meta.env.VITE_BASE_API_URL,
+  BASE_API_URL:
+    import.meta.env.VITE_BASE_API_URL ||
+    "https://inlaco-crewmgr-service-b7btdkgsdwafb2ht.eastasia-01.azurewebsites.net/api",
   CLOUDINARY_API_KEY: import.meta.env.VITE_CLOUDINARY_API_KEY,
   CLOUDINARY_CLOUD_NAME: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
 };
